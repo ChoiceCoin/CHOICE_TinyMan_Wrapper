1d11213be2692797c0014473facb896637ec72b0
B Haney <43055154+Bhaney44@users.noreply.github.com>
2021-11-15 22:10:09 -0800

    Update Choice_TinyMan_Wrapper.py

--- Choice_TinyMan_Wrapper.py
+++ Choice_TinyMan_Wrapper.py
@@ -296,11 +296,41 @@ def choice_for_comand():
             transaction_group = pool.prepare_redeem_transactions(amount)
             transaction_group.sign_with_private_key(address, private_key)
             result = client.submit(transaction_group, wait=True)
+ 
+def choice_for_ACORN():
+    Choice = client.fetch_asset(297995609)
+    ACORN = client.fetch_asset(226265212)
+    pool = client.fetch_pool(Choice, ACORN)
+
+    quote = pool.fetch_fixed_input_swap_quote(ACORN(1_000_000), slippage=0.01)
+    print(quote)
+    print(f'Choice per ACORN: {quote.price * 10000}')
+    print(f'Choice per ACORN (worst case): {quote.price_with_slippage * 10000}')
+    print("Do you still want to go through this transaction? Type Y for 'Yes' and N for 'No'")
+    binary = input()
+    if binary == "Y":
+        new = int(input("How much comand do you want to spend? ")) * 1000000
+        quote = pool.fetch_fixed_input_swap_quote(ACORN(new), slippage=0.01)
+        print(f'Swapping {quote.amount_in} to {quote.amount_out_with_slippage}')
+        transaction_group = pool.prepare_swap_transactions_from_quote(quote)
+        # Sign the group with our key
+        transaction_group.sign_with_private_key(address, private_key)
+        # Submit transactions to the network and wait for confirmation
+        result = client.submit(transaction_group, wait=True)
+        # Check if any excess remaining after the swap
+        excess = pool.fetch_excess_amounts()
+    if Choice in excess:
+        amount = excess[Choice]
+        print(f'Excess: {amount}')
+        if amount > 1_000_000:
+            transaction_group = pool.prepare_redeem_transactions(amount)
+            transaction_group.sign_with_private_key(address, private_key)
+            result = client.submit(transaction_group, wait=True)
 
 user_input = "Go"
 while user_input == "Go":
     # Fix this to add extra ASAs
-    print("What swaps do you want to make with Choice? \n 1. Type 'ALGO' to trade for Choice with Algo. \n 2. Type 'USDC' to trade for Choice with USDC. \n 3. Type 'YLDY' to trade for Choice with Yieldy. \n 4. Type 'OPUL' to trade for Choice with Opulous \n 5. Type 'TACO' to trade for Choice with TACO \n 6. Type 'HDL' to trade for Choice with HDL \n 7. Type 'comand' to trade Choice with command \n 8. Type 'Stop' to stop the program.)
+    print("What swaps do you want to make with Choice? \n 1. Type 'ALGO' to trade for Choice with Algo. \n 2. Type 'USDC' to trade for Choice with USDC. \n 3. Type 'YLDY' to trade for Choice with Yieldy. \n 4. Type 'OPUL' to trade for Choice with Opulous \n 5. Type 'TACO' to trade for Choice with TACO \n 6. Type 'HDL' to trade for Choice with HDL \n 7. Type 'comand' to trade Choice with command \n 8. Type 'comand' to trade Choice with ACORN \n 9. Type 'Stop' to stop the program.)
     user = input()
     if user == "Stop":
         user_input = 'Stop'
@@ -320,3 +350,6 @@ while user_input == "Go":
         choice_for_HDL()
     elif user == 'comand':
         choice_for_comand()
+    elif user == 'ACORN':
+        choice_for_ACORN()
+
